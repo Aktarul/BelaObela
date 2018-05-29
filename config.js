3e45460550175467a49db5ded6829a77aafe834f
Aktarul Islam <aktarul857@gmail.com>
2018-05-29 23:26:04 +0600

    front end 1st commit

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    'database': 'mongodb://localhost:27017/BelaObela'
+    'database': 'mongodb://localhost:27017/BelaObela',
+    'tokenexp': 3600,
+    'secret': '26564654abcd'
 }
