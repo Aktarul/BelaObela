1ef373d42fdf86a3fcdc8ab65297c3e0abbe5e0e
Aktarul Islam <aktarul857@gmail.com>
2018-06-02 02:58:17 +0600

    models and routing module

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import { AuthGaurd} from "./gaurds/auth.gaurd";
+import { ProductComponent} from "./components/product/product.component";
+import { PhotoUploadComponent } from "./components/photo-upload/photo-upload.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {AdminGaurd} from "./gaurds/admin.gaurd";
+import {DetailsComponent} from "./components/details/details.component";
+import {EditproductComponent} from "./components/editproduct/editproduct.component";
+import {EditprofileComponent} from "./components/editprofile/editprofile.component";
+import {AutocompleteFilterExampleComponent} from "./components/autocomplete-filter-example/autocomplete-filter-example.component";
+import { AboutContactComponent } from "./components/about-contact/about-contact.component";
+import {LoginRegisterComponent} from "./components/login-register/login-register.component";
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'dashboard',component:DashboardComponent, canActivate: [AdminGaurd]},
+  {path:'profile',component:ProfileComponent, canActivate: [AuthGaurd] },
+  {path:'product',component:ProductComponent,canActivate: [AdminGaurd]},
+  {path:'photo/:id',component:PhotoUploadComponent},
+  {path:'details/:id',component:DetailsComponent},
+  {path:'cart', component: CartComponent},
+  // {path:'checkout', component: CheckoutComponent, canActivate: [AuthGaurd]},
+  {path:'edit/:id', component: EditproductComponent, canActivate: [AdminGaurd]},
+  {path:'editprofile/:id', component:EditprofileComponent, canActivate: [AuthGaurd]},
+  {path: 'about', component: AboutContactComponent },
+  {path: 'loginregister', component: LoginRegisterComponent},
+  {path:'auto', component:AutocompleteFilterExampleComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
