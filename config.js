a0cdaebb283b2c69bf719dcf25d36bf6c2276de0
aktarul <aktarul857@gmail.com>
2020-11-17 10:55:30 +0600

    outlook changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'database': 'mongodb://localhost:27017/BelaObela',
+    'database': 'mongodb://localhost:27017/BelaObelaNew',
     'tokenexp': 3600,
     'secret': '26564654abcd'
 }
